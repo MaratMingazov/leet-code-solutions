983cee79164a0a4cb9f82270ba4b16dff7537037
Marat Mingazov <marat_mingazov@epam.com>
2022-12-03 22:23:46 +0300

    Add pom dependencies

--- src/main/java/maratmingazovr/leetcode/Application.java
+++ src/main/java/maratmingazovr/leetcode/Application.java
@@ -2,12 +2,14 @@ package maratmingazovr.leetcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-public class LeetcodeApplication {
+@ConfigurationPropertiesScan("maratmingazovr.leetcode.config")
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeetcodeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
