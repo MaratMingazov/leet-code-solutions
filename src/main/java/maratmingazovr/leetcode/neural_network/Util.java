03852fe79f670c7d89cb6f282c110e9ba66bb66e
Marat Mingazov <marat_mingazov@epam.com>
2022-12-14 20:03:56 +0300

    Implement Util methods

--- src/main/java/maratmingazovr/leetcode/neural_network/Util.java
+++ src/main/java/maratmingazovr/leetcode/neural_network/Util.java
@@ -1,6 +1,14 @@
 package maratmingazovr.leetcode.neural_network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Util {
 
@@ -20,6 +28,36 @@ public class Util {
     public static double sigmoidDerivative(double x) {
         double sig = sigmoid(x);
         return sig * (1.0 - sig);
+    }
+
+    // Assume all rows are of equal length
+    // and feature scale each column to be in the range 0 - 1
+    public static void normalizeByFeatureScaling(List<double[]> dataset) {
+        for (int colNum = 0; colNum < dataset.get(0).length; colNum++) {
+            List<Double> column = new ArrayList<>();
+            for (double[] row : dataset) {
+                column.add(row[colNum]);
+            }
+            double maximum = Collections.max(column);
+            double minimum = Collections.min(column);
+            double difference = maximum - minimum;
+            for (double[] row : dataset) {
+                row[colNum] = (row[colNum] - minimum) / difference;
+            }
+        }
+    }
+
+    // Load a CSV file into a List of String arrays
+    public static List<String[]> loadCSV(String filename) {
+        try (InputStream inputStream = Util.class.getResourceAsStream(filename)) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            return bufferedReader.lines().map(line -> line.split(","))
+                                 .collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     // Find the maximum in an array of doubles
