fc62e52184033f1845cb49c299449e559244c634
Marat Mingazov <marat_mingazov@epam.com>
2022-12-16 13:28:56 +0300

    Refactor Neuron class

--- src/main/java/maratmingazovr/leetcode/neural_network/Neuron.java
+++ src/main/java/maratmingazovr/leetcode/neural_network/Neuron.java
@@ -1,16 +1,27 @@
 package maratmingazovr.leetcode.neural_network;
 
 import lombok.NonNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
 public class Neuron {
 
     // First layer neuron has no weights
-    public double[] weights;
-    public final double learningRate;
-    public double outputCache;
-    public double delta;
+    @Nullable
+    List<Double> weights;
+    @Nullable
+    Double biasWeight;
+
+    @NonNull
+    Double learningRate;
+
+    @NonNull
+    Double outputCache;
+
+    @NonNull
+    Double delta;
 
     @NonNull
     final DoubleUnaryOperator activationFunction;
@@ -18,20 +29,28 @@ public class Neuron {
     @NonNull
     final DoubleUnaryOperator derivativeActivationFunction;
 
-    public Neuron(double[] weights,
-                  double learningRate,
+    public Neuron(@Nullable List<Double> weights,
+                  @Nullable Double biasWeight,
+                  @NonNull Double learningRate,
                   @NonNull DoubleUnaryOperator activationFunction,
                   @NonNull DoubleUnaryOperator derivativeActivationFunction) {
         this.weights = weights;
+        this.biasWeight = biasWeight;
         this.learningRate = learningRate;
-        outputCache = 0.0;
-        delta = 0.0;
+        this.outputCache = 0.0;
+        this.delta = 0.0;
         this.activationFunction = activationFunction;
         this.derivativeActivationFunction = derivativeActivationFunction;
     }
 
-    public double calculateOutput(double[] inputs) {
-        outputCache = Util.dotProduct(inputs, weights);
+
+    @NonNull
+    public Double calculateOutput(@NonNull List<Double> inputs) {
+        if (weights == null || biasWeight == null) {
+            throw new IllegalArgumentException("Given neuron has no weights");
+        }
+
+        outputCache = Util.dotProduct(inputs, weights) + biasWeight;
         return activationFunction.applyAsDouble(outputCache);
     }
 }
