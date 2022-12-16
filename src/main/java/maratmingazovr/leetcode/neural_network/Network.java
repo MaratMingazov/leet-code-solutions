6e5cdae496f73f15196deb665be2a314ec2a27a1
Marat Mingazov <marat_mingazov@epam.com>
2022-12-16 14:55:40 +0300

    Refactor Network class

--- src/main/java/maratmingazovr/leetcode/neural_network/Network.java
+++ src/main/java/maratmingazovr/leetcode/neural_network/Network.java
@@ -1,51 +1,75 @@
 package maratmingazovr.leetcode.neural_network;
 
+import lombok.Data;
 import lombok.NonNull;
+import lombok.val;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
 
+@Data
 public class Network<T> {
-    private List<Layer> layers = new ArrayList<>();
 
-    public Network(int[] layerStructure,
-                   double learningRate,
+    @NonNull
+     List<Layer> layers = new ArrayList<>();
+
+    @NonNull
+    Double learningRate;
+
+    public Network(@NonNull List<Integer> layerStructure,
+                   @NonNull Double learningRate,
                    @NonNull DoubleUnaryOperator activationFunction,
                    @NonNull DoubleUnaryOperator derivativeActivationFunction) {
 
-        if (layerStructure.length < 3) {
+        if (layerStructure.size() < 3) {
             throw new IllegalArgumentException("Error: Should be at least 3 layers (1 input, 1 hidden, 1 output).");
         }
+        this.learningRate = learningRate;
+
         // input layer
-        Layer inputLayer = new Layer(Optional.empty(), layerStructure[0], learningRate, activationFunction, derivativeActivationFunction);
+        Layer inputLayer = new Layer(0,
+                                     null,
+                                     layerStructure.get(0),
+                                     List.of(),
+                                     List.of(),
+                                     activationFunction,
+                                     derivativeActivationFunction);
         layers.add(inputLayer);
+
         // hidden layers and output layer
-        for (int i = 1; i < layerStructure.length; i++) {
-            Layer nextLayer = new Layer(Optional.of(layers.get(i - 1)), layerStructure[i], learningRate, activationFunction, derivativeActivationFunction);
+        for (int layerId = 1; layerId < layerStructure.size(); layerId++) {
+            Layer nextLayer = new Layer(layerId,
+                                        layers.get(layerId - 1),
+                                        layerStructure.get(layerId),
+                                        List.of(),
+                                        List.of(),
+                                        activationFunction,
+                                        derivativeActivationFunction);
             layers.add(nextLayer);
         }
     }
 
     // Pushes input data to the first layer, then output from the first
     // as input to the second, second to the third, etc.
-    private double[] calculateOutputs(double[] input) {
-        double[] result = input;
+    @NonNull
+    public  List<Double> calculateOutputs(@NonNull List<Double> input) {
+        var result = input;
         for (Layer layer : layers) {
             result = layer.calculateOutputs(result);
         }
         return result;
-
     }
 
     // Figure out each neuron's changes based on the errors of the output
     // versus the expected outcome
-    private void backpropagate(double[] expected) {
+    @NonNull
+    private void backpropagate(@NonNull List<Double> expected) {
         // calculate delta for output layer neurons
         int lastLayer = layers.size() - 1;
         layers.get(lastLayer).calculateDeltasForOutputLayer(expected);
+
         // calculate delta for hidden layers in reverse order
         for (int i = lastLayer - 1; i >= 0; i--) {
             layers.get(i).calculateDeltasForHiddenLayer(layers.get(i + 1));
@@ -57,9 +81,13 @@ public class Network<T> {
     // actually make changes to the weights
     private void updateWeights() {
         for (Layer layer : layers.subList(1, layers.size())) {
-            for (Neuron neuron : layer.neurons) {
-                for (int w = 0; w < neuron.weights.length; w++) {
-                    neuron.weights[w] = neuron.weights[w] + (neuron.learningRate * layer.previousLayer.get().outputCache[w] * neuron.delta);
+            for (Neuron neuron : layer.getNeurons()) {
+                for (int w = 0; w < neuron.getWeights().size(); w++) {
+                    val previousLayerOutput = layer.getPreviousLayer().getOutputCache().get(w);
+                    val neuronWeights = neuron.getWeights();
+                    val neuronNewWeight = neuronWeights.get(w)+ (learningRate * previousLayerOutput * neuron.delta);
+                    neuronWeights.set(w, neuronNewWeight);
+                    neuron.setBiasWeight(neuron.getBiasWeight() + (learningRate * neuron.delta));
                 }
             }
         }
@@ -67,10 +95,10 @@ public class Network<T> {
 
     // train() uses the results of outputs() run over many inputs and compared
     // against expecteds to feed backpropagate() and updateWeights()
-    public void train(List<double[]> inputs, List<double[]> expecteds) {
+    public void train(List<List<Double>> inputs, List<List<Double>> expecteds) {
         for (int i = 0; i < inputs.size(); i++) {
-            double[] xs = inputs.get(i);
-            double[] ys = expecteds.get(i);
+            val xs = inputs.get(i);
+            val  ys = expecteds.get(i);
             calculateOutputs(xs);
             backpropagate(ys);
             updateWeights();
@@ -92,10 +120,10 @@ public class Network<T> {
     // for generalized results that require classification
     // this function will return the correct number of trials
     // and the percentage correct out of the total
-    public Results validate(List<double[]> inputs, List<T> expecteds, Function<double[], T> interpret) {
+    public Results validate(List<List<Double>> inputs, List<T> expecteds, Function<List<Double>, T> interpret) {
         int correct = 0;
         for (int i = 0; i < inputs.size(); i++) {
-            double[] input = inputs.get(i);
+            val input = inputs.get(i);
             T expected = expecteds.get(i);
             T result = interpret.apply(calculateOutputs(input));
             if (result.equals(expected)) {
