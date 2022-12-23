df697bb0d88e720482da03b83ca0f839f678f7d3
Marat Mingazov <marat_mingazov@epam.com>
2022-12-23 18:29:59 +0300

    Update wineClassificator

--- src/main/java/maratmingazovr/leetcode/tasks/neural_network/wine_classificator/WineClassificator.java
+++ src/main/java/maratmingazovr/leetcode/tasks/neural_network/wine_classificator/WineClassificator.java
@@ -1,10 +1,11 @@
-package maratmingazovr.leetcode.neural_network.wine_classification;
+package maratmingazovr.leetcode.tasks.neural_network.wine_classificator;
 
 import lombok.NonNull;
 import lombok.val;
 import maratmingazovr.leetcode.neural_network.ActivationFunction;
 import maratmingazovr.leetcode.neural_network.Network;
 import maratmingazovr.leetcode.neural_network.Util;
+import maratmingazovr.leetcode.tasks.neural_network.AbstractClassificator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,64 +13,69 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WineClassificator {
+public class WineClassificator extends AbstractClassificator {
 
-    List<List<Double>> inputs = new ArrayList<>();
-    List<List<Double>> expects = new ArrayList<>();
-    private List<Integer> wineSpecies = new ArrayList<>();
 
     public WineClassificator() {
-        val wineDataset = Util.loadCSV("src/main/java/maratmingazovr/leetcode/neural_network/wine_classification/wine.csv");
-        Collections.shuffle(wineDataset);
-
-        for (List<String> data : wineDataset) {
-            // last thirteen items are parameters (doubles)
-            List<Double> input = data.stream().skip(1).map(Double::parseDouble).collect(Collectors.toList());
-            inputs.add(input);
-
-            // first item is species
-            val species = Integer.parseInt(data.get(0));
-            switch (species) {
-                case 1:
-                    expects.add(new ArrayList<Double>(Arrays.asList(1.0, 0.0, 0.0)));
-                    break;
-                case 2:
-                    expects.add(new ArrayList<Double>(Arrays.asList(0.0, 1.0, 0.0)));
-                    break;
-                default:
-                    expects.add(new ArrayList<Double>(Arrays.asList(0.0, 0.0, 1.0)));
-                    break;
-            }
-            wineSpecies.add(species);
-        }
-        Util.normalizeByFeatureScaling(inputs);
+        super("src/main/java/maratmingazovr/leetcode/tasks/neural_network/wine_classificator/data/wine.csv",
+              "src/main/java/maratmingazovr/leetcode/tasks/neural_network/wine_classificator/data/configuration.csv");
     }
 
-    public Network<Integer>.Results classify() {
-        // 4, 6, 3 layer structure; 0.3 learning rate; sigmoid activation function
-        Network<Integer> wineNetwork = new Network<>(List.of(13,7,3), 0.9, ActivationFunction.SIGMOID);
-
-        // train over the first 140 irises in the data set 50 times
-        val inputsTrain = inputs.subList(0, 150);
-        val expectsTrain = expects.subList(0, 150);
-        wineNetwork.train(inputsTrain, expectsTrain, 10L);
-
-        // test over the last 10 of the irises in the data set
-        val inputsTest = inputs.subList(150, 178);
-        val expectsTest = wineSpecies.subList(150, 178);
-        return wineNetwork.validate(inputsTest, expectsTest, this::wineInterpretOutput);
+    @Override
+    public void createDefaultNetworkAndTrain() {
+        network = new Network(List.of(13, 7, 3), 0.9, List.of(ActivationFunction.SIGMOID,ActivationFunction.SIGMOID));
+        loadData();
+        train(50L);
+        saveNetworkConfiguration();
+        loadNetwork();
+        validate();
     }
 
     @NonNull
-    public Integer wineInterpretOutput(@NonNull List<Double> output) {
-        double max = Util.getMaxValueIndex(output);
-        if (max == 0) {
-            return 1;
-        } else if (max == 1) {
-            return 2;
-        } else {
-            return 3;
-        }
+    @Override
+    public Boolean isExpectedEqualToOutput(@NonNull List<Double> expected,
+                                           @NonNull List<Double> output) {
+
+        val expectedMaxValueIndex = Util.getMaxValueIndex(expected);
+        val outputMaxValueIndex = Util.getMaxValueIndex(output);
+        return expectedMaxValueIndex.equals(outputMaxValueIndex);
     }
 
+    @Override
+    public void loadData() {
+
+        val dataset = Util.loadCSV(datasetFile);
+        Collections.shuffle(dataset);
+        List<List<Double>> inputs = new ArrayList<>();
+        List<List<Double>> expects = new ArrayList<>();
+
+        for (List<String> data : dataset) {
+            List<Double> input = data.stream()
+                                     .skip(1)
+                                     .map(Double::parseDouble)
+                                     .collect(Collectors.toList());
+            inputs.add(input);
+
+            val type = data.get(0);
+            switch (type) {
+                case "1":
+                    expects.add(new ArrayList<Double>(Arrays.asList(1.0, 0.0, 0.0)));
+                    break;
+                case "2":
+                    expects.add(new ArrayList<Double>(Arrays.asList(0.0, 1.0, 0.0)));
+                    break;
+                case "3":
+                    expects.add(new ArrayList<Double>(Arrays.asList(0.0, 0.0, 1.0)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid type");
+            }
+        }
+        Util.normalizeByFeatureScaling(inputs);
+
+        inputsTrain.addAll(inputs.subList(0,150));
+        expectsTrain.addAll(expects.subList(0,150));
+        inputsValidate.addAll(inputs.subList(150,178));
+        expectsValidate.addAll(expects.subList(150,178));
+    }
 }
