42706af0755c3cc6c20d8b68d62d49f79e23d60d
Marat Mingazov <marat_mingazov@epam.com>
2023-01-02 12:18:02 +0300

    Implement AnalyzerService

---
+++ src/main/java/maratmingazovr/leetcode/tinkof/AnalyzerService.java
@@ -0,0 +1,358 @@
+package maratmingazovr.leetcode.tinkof;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
+import lombok.val;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.tinkoff.piapi.contract.v1.CandleInterval;
+import ru.tinkoff.piapi.contract.v1.PostOrderResponse;
+import ru.tinkoff.piapi.contract.v1.StopOrder;
+
+import javax.annotation.Nullable;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Service
+@AllArgsConstructor
+public class AnalyzerService {
+
+
+    private final ApiService apiService;
+
+
+    private final TPortfolio portfolio = new TPortfolio();
+
+    @NonNull
+    private final BotService botService;
+
+    @Scheduled(cron = "0 0/1  * * * *") // every minute
+    public void executeEveryMinute() {
+
+
+        val accountId = apiService.getAccountFromApi();
+//        apiService.closeSandboxAccount(accountId);
+//        apiService.openSandboxAccount();
+
+        val interval = CandleInterval.CANDLE_INTERVAL_1_MIN;
+        updateSharesFromApi(interval);
+        calculateMetrics(interval);
+        val sharesToSell = findActiveSharesToSellSandbox(portfolio);
+        sharesToSell.forEach(activeShare -> apiService.sellShareFromApi(accountId, activeShare.getShare().getFigi()));
+        val candlesToBuyLong = findCandlesToBuyLong(portfolio, interval);
+        if (candlesToBuyLong.size() > 0) {
+            log.info("candles to buy = " + candlesToBuyLong.size());
+            buySharesLong(accountId, candlesToBuyLong);
+        }
+        updateOperations(accountId, portfolio);
+    }
+
+    @Scheduled(cron = "0 0/5  * * * *") // every 5 minutes
+    //0 0 0/1 * * * every hour
+    public void executeEvery5Minutes() {
+        log.info("start 5 minute");
+        val accountId = apiService.getAccountFromApi();
+
+        val interval = CandleInterval.CANDLE_INTERVAL_5_MIN;
+        updateSharesFromApi(interval);
+        calculateMetrics(interval);
+        val candlesToBuyLong = findCandlesToBuyLong(portfolio, interval);
+        if (candlesToBuyLong.size() > 0) {
+            log.info("candles to buy = " + candlesToBuyLong.size());
+            buySharesLong(accountId, candlesToBuyLong);
+        }
+        log.info("finish 5 minute");
+    }
+
+    @Scheduled(cron = "0 0/15  * * * *") // every 15 minutes
+    //0 0 0/1 * * * every hour
+    public void executeEvery15Minutes() {
+        log.info("start 15 minute");
+        val accountId = apiService.getAccountFromApi();
+
+        val interval = CandleInterval.CANDLE_INTERVAL_15_MIN;
+        updateSharesFromApi(interval);
+        calculateMetrics(interval);
+        val candlesToBuyLong = findCandlesToBuyLong(portfolio, interval);
+        if (candlesToBuyLong.size() > 0) {
+            log.info("candles to buy = " + candlesToBuyLong.size());
+            buySharesLong(accountId, candlesToBuyLong);
+        }
+        log.info("finish 15 minute");
+    }
+
+    @Scheduled(cron = "0 0 0/1 * * *") // every 1 hour
+    //0 0 0/1 * * * every hour
+    public void executeEvery1Hour() {
+        log.info("start 1 hour");
+        val accountId = apiService.getAccountFromApi();
+        val interval = CandleInterval.CANDLE_INTERVAL_HOUR;
+        updateSharesFromApi(interval);
+        calculateMetrics(interval);
+        val candlesToBuyLong = findCandlesToBuyLong(portfolio, interval);
+        if (candlesToBuyLong.size() > 0) {
+            log.info("candles to buy = " + candlesToBuyLong.size());
+            buySharesLong(accountId, candlesToBuyLong);
+        }
+        log.info("finish 1 hour");
+    }
+
+    private synchronized void buySharesLong(@NonNull String accountId,
+                                            @NonNull List<TCandle> candles) {
+        for (TCandle candle : candles) {
+            if(!candle.getShare().getActiveShares().isEmpty()) {
+                continue;
+            }
+            val figi = candle.getShare().getFigi();
+            val order = apiService.buyShareFromApi(accountId, figi);
+            val comission = TUtils.moneyValueToDouble(order.getExecutedCommission());
+            val comissionCurrency = order.getExecutedCommission().getCurrency();
+            val price = TUtils.moneyValueToDouble(order.getExecutedOrderPrice());
+            val takeProfit = price + 0.02 * price;
+            val stopLoss = price - 0.02 * price;
+            val sma = String.format("%.2f", candle.getSimpleMovingAverage());
+            val bollingerUp = String.format("%.2f", candle.getBollingerUp());
+            val bollingerDown = String.format("%.2f", candle.getBollingerDown());
+            candle.getShare().setLastShareStopLoss(stopLoss);
+            candle.getShare().setLastShareTakeProfit(takeProfit);
+            candle.getShare().setLastSharePrice(price);
+            candle.getShare().setLastSharePosition("LONG");
+            candle.getShare().setLastShareComission(comission);
+            candle.getShare().setLastShareComissionCurrency(comissionCurrency);
+            candle.getShare().setLastShareSMA(sma);
+            candle.getShare().setLastShareBollingerUp(bollingerUp);
+            candle.getShare().setLastShareBollingerDown(bollingerDown);
+            candle.getShare().setLastShareInterval(candle.getInterval().toString());
+            //val stopLoss = apiService.stopLossOrder(accountId, figi, orderPrice);
+            //val takeProfit = apiService.takeProfitOrder(accountId, figi, orderPrice);
+            //val message = generateBuyShareMessage(order, stopLoss, takeProfit, candle, "LONG", Instant.now());
+            //val message = generateBuyShareMessageSandbox(order, stopLoss, takeProfit, candle, "LONG", Instant.now());
+            //log.info(message);
+            //botService.sendMassage(message);
+        }
+    }
+
+    public String getPortfolio() {
+        val accountId = apiService.getAccountFromApi();
+        apiService.updatePortfolioFromApi(accountId, portfolio);
+        return generatePortfolioMessage(portfolio);
+    }
+
+    private synchronized void updateOperations(@NonNull String accountId,
+                                               @NonNull TPortfolio portfolio) {
+        var from = Instant.now().minus(1, ChronoUnit.DAYS);
+        val operations = portfolio.getOperations();
+        if (portfolio.getOperations().size() > 0) {
+            val lastOperation = operations.get(operations.size() - 1);
+            from = lastOperation.getInstant();
+        }
+
+        val newOperations = apiService.getOperationsFromApi(accountId, from, portfolio);
+        val currentOperations = portfolio.getOperations();
+        if (currentOperations.isEmpty()) {
+            currentOperations.addAll(newOperations);
+            currentOperations.forEach(operation -> botService.sendMassage(operation.toString()));
+        } else {
+            val lastOperation = operations.get(operations.size() - 1);
+            for (TOperation newOperation : newOperations) {
+                if (newOperation.getInstant().isAfter(lastOperation.getInstant())) {
+                    operations.add(newOperation);
+                    botService.sendMassage(newOperation.toString());
+                }
+            }
+        }
+
+        while(operations.size() > 100) {
+            operations.remove(0);
+        }
+    }
+
+    private List<TActiveShare> findActiveSharesToSellSandbox(@NonNull TPortfolio portfolio) {
+        List<TActiveShare> result = new ArrayList<>();
+        for (TShare share : portfolio.getShares()) {
+            for (TActiveShare activeShare : share.getActiveShares()) {
+                if (activeShare.getPrice() > share.getLastShareTakeProfit() || activeShare.getPrice() < share.getLastShareStopLoss()) {
+                    result.add(activeShare);
+                }
+            }
+        }
+        return result;
+    }
+
+    private String generateBuyShareMessageSandbox(@NonNull PostOrderResponse order,
+                                           @NonNull Double stopLoss,
+                                           @NonNull Double takeProfit,
+                                           @NonNull TCandle candle,
+                                           @NonNull String position,
+                                           @NonNull Instant instant) {
+
+        val price = TUtils.moneyValueToDouble(order.getExecutedOrderPrice());
+        val priceCurrency = order.getExecutedOrderPrice().getCurrency();
+        val comission = TUtils.moneyValueToDouble(order.getExecutedCommission());
+        val comissionCurrency = order.getExecutedCommission().getCurrency();
+        val takeProfitPrice = String.format("%.2f", takeProfit);
+        val stopLossPrice = String.format("%.2f", stopLoss);
+        val sma = String.format("%.2f", candle.getSimpleMovingAverage());
+        val bollingerUp = String.format("%.2f", candle.getBollingerUp());
+        val bollingerDown = String.format("%.2f", candle.getBollingerDown());
+
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy:hh:mm").withZone(ZoneId.systemDefault());
+
+        return "Buy:" + "\n"
+                + "id: " + candle.getShare().getId() + "\n"
+                + "position: " + position + "\n"
+                + "date: " + formatter.format(instant) + "\n"
+                + "price: " + price + " " + priceCurrency + "\n"
+                + "comission: " + comission + " " + comissionCurrency + "\n"
+                + "takeProfit: " + takeProfitPrice + "\n"
+                + "stopLoss: " + stopLossPrice + "\n"
+                + "BB: " + sma + " " + bollingerUp + " " + bollingerDown + "\n";
+    }
+
+    private String generateBuyShareMessage(@NonNull PostOrderResponse order,
+                                           @Nullable StopOrder stopLoss,
+                                           @Nullable StopOrder takeProfit,
+                                           @NonNull TCandle candle,
+                                           @NonNull String position,
+                                           @NonNull Instant instant) {
+
+        val price = TUtils.moneyValueToDouble(order.getExecutedOrderPrice());
+        val priceCurrency = order.getExecutedOrderPrice().getCurrency();
+        val comission = TUtils.moneyValueToDouble(order.getExecutedCommission());
+        val comissionCurrency = order.getExecutedCommission().getCurrency();
+//        val takeProfitPrice = TUtils.moneyValueToDouble(takeProfit.getPrice());
+//        val takeProfitCurrency = takeProfit.getCurrency();
+//        val takeProfitExpiration = timestampToString(takeProfit.getExpirationTime());
+//        val stopLossPrice = TUtils.moneyValueToDouble(stopLoss.getPrice());
+//        val stopLossCurrency = stopLoss.getCurrency();
+//        val stopLossExpiration = timestampToString(stopLoss.getExpirationTime());
+        val sma = String.format("%.2f", candle.getSimpleMovingAverage());
+        val bollingerUp = String.format("%.2f", candle.getBollingerUp());
+        val bollingerDown = String.format("%.2f", candle.getBollingerDown());
+
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy:hh:mm").withZone(ZoneId.systemDefault());
+
+        return "Buy:" + "\n"
+                + "id: " + candle.getShare().getId() + "\n"
+                + "position: " + position + "\n"
+                + "date: " + formatter.format(instant) + "\n"
+                + "price: " + price + " " + priceCurrency + "\n"
+                + "comission: " + comission + " " + comissionCurrency + "\n"
+//                + "takeProfit: " + takeProfitPrice + " " + takeProfitCurrency + " " + takeProfitExpiration + "\n"
+//                + "stopLoss: " + stopLossPrice + " " + stopLossCurrency + " " + stopLossExpiration + "\n"
+                + "BB: " + sma + " " + bollingerUp + " " + bollingerDown + "\n";
+    }
+
+    private String generatePortfolioMessage(@NonNull TPortfolio portfolio) {
+        int totalCandlesCount = 0;
+        for (TShare share : portfolio.getShares()) {
+            val candles = share.getCandlesMap().values();
+            for (List<TCandle> candle : candles) {
+                totalCandlesCount += candle.size();
+            }
+        }
+        val activeShares = portfolio.getShares().stream().flatMap(share -> share.getActiveShares().stream()).collect(Collectors.toList());
+        StringBuilder result = new StringBuilder()
+                .append("Balance:" + "\n")
+                .append("USD: " + String.format("%.2f", portfolio.getDollarBalance()) + "\n")
+                .append("RUB: " + String.format("%.2f", portfolio.getRubBalance()) + "\n")
+                .append("operations: " + portfolio.getOperations().size() + "\n")
+                .append("candlesCount: " + totalCandlesCount + "\n");
+
+        result.append("SHARES: \n");
+        for (TActiveShare activeShare : activeShares) {
+            result.append(activeShare.getShare().getId())
+                  .append(" count: ").append(activeShare.getCount())
+                  .append(" price: ").append(String.format("%.2f", activeShare.getPrice()))
+                  .append(" ").append(activeShare.getCurrency()).append("\n");
+        }
+        return result.toString();
+    }
+
+    private synchronized void updateSharesFromApi(@NonNull CandleInterval interval) {
+        int periodsCount = 100;
+        Instant defaultFrom;
+        switch (interval) {
+            case CANDLE_INTERVAL_1_MIN:
+                defaultFrom = Instant.now().minus(periodsCount, ChronoUnit.MINUTES);
+                break;
+            case CANDLE_INTERVAL_5_MIN:
+                defaultFrom = Instant.now().minus(periodsCount * 5, ChronoUnit.MINUTES);
+                break;
+            case CANDLE_INTERVAL_15_MIN:
+                defaultFrom = Instant.now().minus(  20, ChronoUnit.HOURS);
+                break;
+            case CANDLE_INTERVAL_HOUR:
+                defaultFrom = Instant.now().minus(22, ChronoUnit.HOURS);
+                break;
+            case CANDLE_INTERVAL_DAY:
+                defaultFrom = Instant.now().minus(periodsCount, ChronoUnit.DAYS);
+                //defaultFrom = Instant.now().minus(100, ChronoUnit.DAYS);
+                break;
+            default: throw new IllegalArgumentException("Invalid candleInterval");
+        }
+        portfolio.getShares().forEach(share -> addNewCandlesFromApi(share, interval, defaultFrom));
+    }
+
+    private void addNewCandlesFromApi(@NonNull TShare share,
+                                      @NonNull CandleInterval interval,
+                                      @NonNull Instant defaultFrom) {
+
+        var from = defaultFrom;
+        val existingCandles = share.getCandlesMap().get(interval);
+        if (!existingCandles.isEmpty()) {
+            from = existingCandles.get(existingCandles.size()-1).getInstant();
+        }
+        val newCandles = apiService.getCandlesFromApi(share, from, interval);
+        val candles = share.getCandlesMap().get(interval);
+        candles.addAll(newCandles);
+        while(candles.size() > 100) {
+            candles.remove(0);
+        }
+    }
+
+    private void calculateMetrics(@NonNull CandleInterval interval) {
+        portfolio.getShares().forEach(share -> {
+            TUtils.calculateSimpleMovingAverage(share, interval);
+            TUtils.calculateBollingerUpAndDown(share, interval);
+        } );
+    }
+
+    @NonNull
+    private List<TCandle> findCandlesToBuyLong(@NonNull TPortfolio portfolio,
+                                               @NonNull CandleInterval interval) {
+        List<TCandle> candlesToBuy = new ArrayList<>();
+        for (TShare share : portfolio.getShares()) {
+            if (!share.getActiveShares().isEmpty()) {
+                continue;
+            }
+            val candleToButOpt = findCandleToBuyLong(share.getCandlesMap().get(interval));
+            candleToButOpt.ifPresent(candlesToBuy::add);
+        }
+        return candlesToBuy;
+    }
+
+    @NonNull
+    private Optional<TCandle> findCandleToBuyLong(List<TCandle> candles) {
+        if (candles.isEmpty()) {
+            // we have no candles to check
+            return Optional.empty();
+        }
+        val lastCandle = candles.get(candles.size() - 1);
+        if (lastCandle.getBollingerDown() == null) {
+            return Optional.empty();
+        }
+        if (lastCandle.getLow() < lastCandle.getBollingerDown()) {
+            return Optional.of(lastCandle);
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/maratmingazovr/leetcode/tinkof/ApiService.java
@@ -0,0 +1,197 @@
+package maratmingazovr.leetcode.tinkof;
+
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.tinkoff.piapi.contract.v1.Account;
+import ru.tinkoff.piapi.contract.v1.CandleInterval;
+import ru.tinkoff.piapi.contract.v1.HistoricCandle;
+import ru.tinkoff.piapi.contract.v1.MoneyValue;
+import ru.tinkoff.piapi.contract.v1.OrderDirection;
+import ru.tinkoff.piapi.contract.v1.OrderType;
+import ru.tinkoff.piapi.contract.v1.PostOrderResponse;
+import ru.tinkoff.piapi.contract.v1.Quotation;
+import ru.tinkoff.piapi.contract.v1.StopOrder;
+import ru.tinkoff.piapi.contract.v1.StopOrderDirection;
+import ru.tinkoff.piapi.contract.v1.StopOrderType;
+import ru.tinkoff.piapi.core.InvestApi;
+import ru.tinkoff.piapi.core.models.Position;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Service
+public class ApiService {
+
+    private final InvestApi api;
+
+    private String accountId;
+
+    public ApiService(@Autowired InvestApi api) {
+        this.api = api;
+    }
+
+    public synchronized  void closeSandboxAccount(@NonNull String accountId) {
+        api.getSandboxService().closeAccountSync(accountId);
+    }
+
+    public synchronized void openSandboxAccount() {
+        String accountId = api.getSandboxService().openAccountSync();
+        log.info("открыт новый аккаунт в песочнице {}", accountId);
+        var accounts = api.getUserService().getAccountsSync();
+        Account mainAccount = accounts.get(0);
+        for (Account account : accounts) {
+            log.info("sandbox account id: {}, access level: {}", account.getId(), account.getAccessLevel().name());
+        }
+        //Убеждаемся, что мы в режиме песочницы
+        log.info("тариф должен быть sandbox. фактический тариф: {}", api.getUserService().getInfoSync().getTariff());
+
+        //пополняем счет песочницы на 10_000 рублей и 10_000 долларов
+        api.getSandboxService().payInSync(mainAccount.getId(), MoneyValue.newBuilder().setUnits(10000).setCurrency("RUB").build());
+        api.getSandboxService().payInSync(mainAccount.getId(), MoneyValue.newBuilder().setUnits(10000).setCurrency("USD").build());
+    }
+
+    @NonNull
+    public List<TCandle> getCandlesFromApi(@NonNull TShare share,
+                                               @NonNull Instant from,
+                                               @NonNull CandleInterval interval) {
+        List<HistoricCandle> candles = api.getMarketDataService().getCandlesSync(share.getFigi(), from, Instant.now(), interval);
+        return candles.stream().map(hCandle -> new TCandle(hCandle, share, interval)).collect(Collectors.toList());
+    }
+
+    @NonNull
+    public PostOrderResponse buyShareFromApi(@NonNull String accountId,
+                                             @NonNull String shareFigi) {
+
+        Quotation lastPrice = api.getMarketDataService().getLastPricesSync(List.of(shareFigi)).get(0).getPrice();
+
+        //Выставляем заявку на покупку по рыночной цене
+        return api.getOrdersService().postOrderSync(shareFigi,
+                                                    1,
+                                                    lastPrice,
+                                                    OrderDirection.ORDER_DIRECTION_BUY,
+                                                    accountId,
+                                                    OrderType.ORDER_TYPE_MARKET,
+                                                    UUID.randomUUID().toString());
+    }
+
+    @NonNull
+    public PostOrderResponse sellShareFromApi(@NonNull String accountId,
+                                              @NonNull String shareFigi) {
+        Quotation lastPrice = api.getMarketDataService().getLastPricesSync(List.of(shareFigi)).get(0).getPrice();
+
+        //Выставляем заявку на продажу по рыночной цене
+        return api.getOrdersService().postOrderSync(shareFigi,
+                                                    1,
+                                                    lastPrice,
+                                                    OrderDirection.ORDER_DIRECTION_SELL,
+                                                    accountId,
+                                                    OrderType.ORDER_TYPE_MARKET,
+                                                    UUID.randomUUID().toString());
+    }
+
+    public synchronized void updatePortfolioFromApi(@NonNull String accountId,
+                                      @NonNull TPortfolio portfolio) {
+        val portfolioSync = api.getOperationsService().getPortfolioSync(accountId);
+        val positions = portfolioSync.getPositions();
+        for (TShare share : portfolio.getShares()) {
+            share.getActiveShares().clear();
+        }
+        for (Position position : positions) {
+            val figi = position.getFigi();
+            double count = position.getQuantity().doubleValue();
+            if (count <= 0) {
+                continue;
+            }
+            val price = position.getCurrentPrice().getValue().doubleValue();
+            val currency = position.getCurrentPrice().getCurrency();
+            if (figi.equals(portfolio.getDollarBalanceFigi())) {
+                portfolio.setDollarBalance(count);
+            }
+            if (figi.equals(portfolio.getRubBalanceFigi())) {
+                portfolio.setRubBalance(count);
+            }
+            for (TShare share : portfolio.getShares()) {
+                if (figi.equals(share.getFigi())) {
+                    share.getActiveShares().add(new TActiveShare(currency, price, count, share));
+                }
+            }
+        }
+    }
+
+    @NonNull
+    public synchronized String getAccountFromApi() {
+        if (accountId == null) {
+            var accounts = api.getUserService().getAccountsSync();
+            accountId =  accounts.get(0).getId();
+        }
+        return accountId;
+    }
+
+    @NonNull
+    public  StopOrder stopLossOrder(@NonNull String accountId,
+                                     @NonNull String figi,
+                                     @NonNull MoneyValue sharePrice) {
+
+        var stopPrice = Quotation.newBuilder()
+                                 .setUnits(sharePrice.getUnits() - sharePrice.getUnits() * 100 / 5)
+                                 .setNano(sharePrice.getNano() - sharePrice.getNano() * 100 / 5)
+                                 .build();
+
+        var stopOrderId = api.getStopOrdersService()
+                             .postStopOrderGoodTillDateSync(figi,
+                                                            1,
+                                                            stopPrice,
+                                                            stopPrice,
+                                                            StopOrderDirection.STOP_ORDER_DIRECTION_SELL,
+                                                            accountId,
+                                                            StopOrderType.STOP_ORDER_TYPE_STOP_LOSS,
+                                                            Instant.now().plus(30, ChronoUnit.DAYS));
+
+
+        //Получаем список стоп-заявок и смотрим, что наша заявка в ней есть
+        var stopOrders = api.getStopOrdersService().getStopOrdersSync(accountId);
+        return stopOrders.stream().filter(el -> el.getStopOrderId().equals(stopOrderId)).findAny().orElseThrow();
+    }
+
+    @NonNull
+    public  StopOrder takeProfitOrder(@NonNull String accountId,
+                                       @NonNull String figi,
+                                       @NonNull MoneyValue sharePrice) {
+
+        var stopPrice = Quotation.newBuilder()
+                                 .setUnits(sharePrice.getUnits() + sharePrice.getUnits() * 100 / 5)
+                                 .setNano(sharePrice.getNano() + sharePrice.getNano() * 100 / 5)
+                                 .build();
+
+        var stopOrderId = api.getStopOrdersService()
+                             .postStopOrderGoodTillDateSync(figi,
+                                                            1,
+                                                            stopPrice,
+                                                            stopPrice,
+                                                            StopOrderDirection.STOP_ORDER_DIRECTION_SELL,
+                                                            accountId,
+                                                            StopOrderType.STOP_ORDER_TYPE_TAKE_PROFIT,
+                                                            Instant.now().plus(1, ChronoUnit.YEARS));
+
+        //Получаем список стоп-заявок и смотрим, что наша заявка в ней есть
+        var stopOrders = api.getStopOrdersService().getStopOrdersSync(accountId);
+        return stopOrders.stream().filter(el -> el.getStopOrderId().equals(stopOrderId)).findAny().orElseThrow();
+    }
+
+    @NonNull
+    public List<TOperation> getOperationsFromApi(@NonNull String accountId,
+                                                 @NonNull Instant from,
+                                                 @NonNull TPortfolio portfolio) {
+        return api.getOperationsService()
+                            .getAllOperationsSync(accountId, from, Instant.now())
+                .stream().map(operation -> new TOperation(operation, portfolio))
+                .collect(Collectors.toList());
+    }
+}
